96f8ce79d707caafee3c3b68e8c612b22f0af60f
Eugene Scherba <escherba@gmail.com>
2017-02-07 23:29:25 -0800

    use amqp protocol

--- tasks.py
+++ tasks.py
@@ -3,15 +3,8 @@ from celery import Celery
 from app import app
 
 
-"""
 app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379/0',
-    CELERY_RESULT_BACKEND='redis://localhost:6379/0'
-)
-"""
-
-app.config.update(
-    CELERY_BROKER_URL='pyamqp://guest@localhost//',
+    CELERY_BROKER_URL='amqp://guest@localhost//',
     CELERY_RESULT_BACKEND='rpc://'
 )
 
